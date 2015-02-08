bfa5c90ab6327c5397763722ed528c260188ed5d
BizarreKitten <senxx041@morris.umn.edu>
2015-02-08 16:46:23 -0600

    finished and tested gpa calculation functions

--- client/views/gpa/gpa.controller.js
+++ client/views/gpa/gpa.controller.js
@@ -15,18 +15,16 @@ angular.module('appModule')
         $scope.data = [];
 
         $scope.getGpa = function(){
-            console.log($scope.data);
             $http.get('api/gpa').success(function(gpa) {
                 $scope.data = gpa;
             });
-            console.log($scope.data);
         };
 
         $scope.getGpa();
 
         $scope.addData = function(){
             if($scope.classField.length >= 1 && $scope.gradeField.length >= 1 && $scope.creditField.length >= 1) {
-                //$scope.data.push({class: $scope.classField, grade: $scope.gradeField, credits: parseInt($scope.creditField)});
+                $scope.data.push({class: $scope.classField, grade: $scope.gradeField, credits: parseInt($scope.creditField)});
                 $http.post('api/gpa', {class: $scope.classField, grade: $scope.gradeField, credits: parseInt($scope.creditField)}).success(function(){
                     $scope.getGpa();
                 });
@@ -51,7 +49,42 @@ angular.module('appModule')
             return $scope.data.length;
         };
 
-        $scope.toNumber = function(){
-
+        $scope.toNumber = function(letter){
+            var gradeNum = 0;
+            if(letter == 'A' || letter == 'a'){
+                gradeNum = 4;
+            }
+            else if(letter == 'B' || letter == 'b'){
+                gradeNum = 3;
+            }
+            else if(letter == 'C' || letter == 'c'){
+                gradeNum = 2;
+            }
+            else if(letter == 'D' || letter == 'd'){
+                gradeNum = 1;
+            }
+            else if(letter == 'F' || letter == 'f'){
+                gradeNum = 0;
+            }
+            else{
+                alert('INVALID ENTRY')
+                return;
+            }
+            return gradeNum;
         };
+
+        $scope.returnGpa = function (){
+            var gradeSum = 0;
+            var creditSum = 0;
+
+            for(var i = 0; i < $scope.data.length; i++){
+                gradeSum += (Number($scope.toNumber($scope.data[i].grade)) * Number($scope.data[i].credits));
+                creditSum += Number($scope.data[i].credits);
+                console.log(gradeSum);
+                console.log(creditSum);
+            }
+
+            var final = gradeSum / creditSum;
+            return final;
+        }
     });
--- client/views/gpa/gpa.controller.spec.js
+++ client/views/gpa/gpa.controller.spec.js
@@ -1,3 +1,47 @@
 /**
  * Created by senxx041 on 2/8/15.
  */
+'use strict';
+
+//=== Testing gpaCtrl =============================================
+describe('Testing controller: gpaCtrl', function(){
+
+    // load the controller's module
+    beforeEach(module('appModule'));
+
+    var gpaCtrl, scope;
+
+    // Initialize the controller and mock scope.
+    beforeEach(inject(function($controller, $rootScope) {
+        scope = $rootScope.$new();
+        gpaCtrl = $controller('gpaCtrl', {
+            $scope: scope
+        });
+
+
+    }));
+
+    it('dummy test should pass', function(){
+        expect(true).toEqual(true);
+    });
+
+    it('should change letter grades to point values', function(){
+        expect(scope.toNumber('A')).toEqual(4);
+        expect(scope.toNumber('C')).toEqual(2);
+        expect(scope.toNumber('F')).toEqual(0);
+    });
+
+    it('should return the gpa', function(){
+        scope.classField = 'class uno';
+        scope.gradeField = 'A';
+        scope.creditField = '5';
+        scope.addData();
+        scope.classField = 'class dos';
+        scope.gradeField = 'B';
+        scope.creditField = '5';
+        scope.addData();
+        expect(scope.returnGpa()).toEqual(3.5);
+    });
+
+
+});
